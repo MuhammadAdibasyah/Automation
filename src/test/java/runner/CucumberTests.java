3a824caa9d405b8de3d9c37d4425e9b17884b6d6
adibasyah <mhmmd.dedeb@gmail.com>
2024-12-02 15:49:17 +0700

    tugas modul CI/CD revisi code web 2

--- src/test/java/runner/CucumberTests.java
+++ src/test/java/runner/CucumberTests.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        glue = {"stepdef"},
+        glue = {"stepDef"},
         features = {"src/test/java/features"},
         plugin = {"pretty" , "html:reports/cucumber.html","json:reports/cucumber.json"},
         tags = "@Web"
--- src/test/java/stepDef/LoginStep.java
+++ src/test/java/stepDef/LoginStep.java
@@ -1,4 +1,4 @@
-package stepdef;
+package stepDef;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
