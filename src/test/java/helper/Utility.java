9306715c12cac2f26c076499f1b18f07943edeee
adibasyah <mhmmd.dedeb@gmail.com>
2024-12-03 12:49:33 +0700

    tugas modul CI/CD revisi code web 3

--- src/test/java/helper/Utility.java
+++ src/test/java/helper/Utility.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.time.Duration;
 
@@ -13,12 +15,15 @@ public class Utility {
 
     public static void startDriver(){
         ChromeOptions options = new ChromeOptions();
+        FirefoxOptions options1 = new FirefoxOptions();
 //        options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--remote-allow-origins=*");
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver(options);
+//        WebDriverManager.chromedriver().setup();
+//        driver = new ChromeDriver(options);
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver(options1);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
     }
